9373131b94cbd5a3b25142ead9a995129d3a8f83
Luke Borwell <lborwell@gmail.com>
2013-11-13 01:11:37 +0000

    random lists, mergesort, quicksort

--- src/loop.c
+++ src/loop.c
@@ -5,19 +5,8 @@ int main();
 int f(int);
 
 int main(){
-	int i,j,k;
-	/*for(i = 0; i<100000000; i++){
-        int x = i*293 + f(x);
-		printf("%i",x);
+	int i;
+	for(i = 0; i<100000000; i++){
+		printf("%i",i);
 	}
-	puts("");*/
-    for(i=0; i<1000; i++)
-        for(j=0; j<100; j++)
-            for(k=0; k<100; k++)
-                printf("%i,%i,%i",i,j,k);
-	return 0;
-}
-
-int f(int x){
-    return x + x + x - x%2;
 }
