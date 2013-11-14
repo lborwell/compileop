d0b4ab0a22c00c6b6257138afbd4d457cf6358d9
Luke Borwell <lborwell@gmail.com>
2013-11-14 18:58:07 +0000

    removing files

--- clear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SPLIT="============================="
-echo $SPLIT
-echo "Clear"
-
-rm -rf bin
-rm results.csv
-echo $SPLIT
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source clear.sh
-source compile.sh
-source test.sh
